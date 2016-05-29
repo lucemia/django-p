c3f03840c5c0a31f18a4ba94b72c8491be63b64e
lucemia <lucemia@gmail.com>
2016-05-29 17:23:46 +0800

    v0.01

--- django_p/models.py
+++ django_p/models.py
@@ -1,68 +1,81 @@
 from django.db import models
 from jsonfield import JSONField
-from aenum import Enum
+from model_utils import Choices
 from . import util
 
 # Create your models here.
 
-def _choices(enum):
-    choices = []
-    for x in dir(enum):
-        if x.startswith('_'):
-            continue
-        else:
-            choices.append((getattr(enum, x), x))
-
-    return choices
-
-
 class Pipeline(models.Model):
-    STATUS = Enum("STATUS", "ABORTED DONE RUN WAITING")
+    STATUS = Choices(
+        (3, 'WAITING'),
+        (2, 'RUN'),
+        (1, 'DONE'),
+        (0, 'ABORTED')
+    )
 
     class_path = models.CharField(max_length=255)
-    root_pipeline = models.ForeignKey("Pipeline")
+    root_pipeline = models.ForeignKey("Pipeline", null=True, blank=True)
+    parent_pipeline = models.ForeignKey("Pipeline", null=True, blank=True)
 
-    fanned_out = models.ManyToManyField("Pipeline")
     started = models.DateTimeField(auto_now_add=True)
     finalized = models.DateTimeField(auto_now=True)
 
     params = JSONField(defaults={})
-    status = models.IntegerField(choices=_choices(STATUS), default=STATUS.WAITING)
+    status = models.IntegerField(choices=STATUS, default=STATUS.WAITING)
 
     current_attempt = models.IntegerField(default=0)
     max_attempts = models.IntegerField(default=1)
     next_retry_time = models.DateTimeField()
     retry_message = models.TextField()
 
-    is_root_pipeline = models.BooleanField(default=False)
     abort_message = models.TextField()
     abort_requested = models.BooleanField(default=False)
 
-    def get_pipe():
-        pass
+    @property
+    def is_root_pipeline(self):
+        return self.root_pipeline == None
+
+    @classmethod
+    def get(cls, pk=None):
+        # TODO:
+        raise NotImplementedError()
 
 
 class Slot(models.Model):
-    STATUS = Enum("STATUS", "WAITING FILLED")
+    STATUS = Choices(
+        (1, 'FILLED'),
+        (0, 'WAITING')
+    )
 
-    root_pipeline = models.ForeignKey(Pipeline)
     filler = models.ForeignKey(Pipeline)
+    name = models.CharField(max_length=255)
 
     value = JSONField(default={})
-    status = models.IntegerField(choices=_choices(STATUS), default=STATUS.WAITING)
+    status = models.IntegerField(choices=STATUS, default=STATUS.WAITING)
 
     filled = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        unique_together = (('filler', 'name'), )
+
 
 class Barrier(models.Model):
-    STATUS = Enum("STATUS", "START FINALIZE ABORT")
+    PURPOSE = Choices(
+        (2, "ABORT"),
+        (1, "FINALIZE"),
+        (0, "START")
+    )
 
-    root_pipeline = models.ForeignKey(Pipeline)
+    STATUS = Choices(
+        (1, "FIRED"),
+        (0, "WAITING")
+    )
+
     target = models.ForeignKey(Pipeline)
 
     blocking_slots = models.ManyToManyField(Slot)
-    triggered = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=_choices(STATUS), default=STATUS.WAITING)
+    triggered = models.DateTimeField()
+    status = models.IntegerField(choices=STATUS, default=STATUS.WAITING)
 
 
 class Status(models.model):
@@ -70,135 +83,3 @@ class Status(models.model):
     message = models.TextField()
 
     updated = models.DateTimeField(auto_now=True)
-
-
-class Pipe(object):
-    # may use proxy model of Pipeline
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-        self.outputs = None
-        self._context = None
-
-    @classmethod
-    def from_id(cls, pipeline_id):
-        # TODO:
-        pass
-
-    def start(self):
-        self._context = PipeContext()
-        self.outputs = PipeFuture(self.outputs)
-
-        return self._context.start(self)
-
-    def run(self, *args, **kwargs):
-        raise NotImplementedError()
-
-
-class PipeFuture(object):
-    def __init__(self, output_names):
-        self._after_all_pipelines = set()
-        self._output_dict = {
-            'default': Slot(name='default')
-        }
-
-        for name in output_names:
-            if name in self._output_dict:
-                raise
-            self._output_dict[name] = Slot(name=name)
-
-    def _inherit_outputs(self, pipeline_name):
-        pass
-
-    def __getattr__(self, name):
-        if name not in self._output_dict:
-            raise
-        return self._output_dict[name]
-
-
-class PipeContext(object):
-
-    def fill_slot(self, filler, slot, value):
-        # do in transaction
-
-        slot.filler = filler
-        self.value = value
-        self.status = Slot.STATUS.FILLED
-        self.save()
-
-        # call notify barrier async
-        # barrier handler
-
-        self.notify_barriers(slot.pk)
-
-    def notify_barriers(self, slot_key):
-        slot = Slot.objects.get(id=slot_key)
-
-        results = slot.barrier_set.all()
-
-        for barrier in results:
-            ready_slots = []
-            for block_slot in barrier.blocking_slots:
-                if block_slot.status == Slot.STATUS.FILLED:
-                    ready_slots.append(block_slot)
-
-            pending_slots = set(barrier.blocking_slots) - set(ready_slots)
-
-            if not pending_slots:
-                if barrier.status != Barrier.STATUS.FIRED:
-                    barrier.status = Barrier.STATUS.FIRED
-                    barrier.triggered = _gettime()
-                    barrier.save()
-
-                purpose = barrier.purpose
-                run_pipeline(barrier.target.pk)
-
-    # abort
-
-    def start(self, pipe):
-        for name, slot in pipe.outputs._output_dict.iteritems():
-            pass
-
-        # TODO: create pipeline object from pipe
-        pipeline = None
-
-        bulk_create = []
-        for name, slot in pipe.outputs._output_dict.iteritems():
-            bulk_create.append(Slot(
-                root_pipeline=pipeline
-            ))
-
-        bulk_create.extend(self._create_barrier_entities(
-            output_slots
-        ))
-
-        models.Models.bulk_create(bulk_create)
-
-        tasks.run_pipeline()
-
-
-    def evaluate(self, pipeline_pk):
-        pipeline_record = Pipeline.objects.get(pk=pipeline_pk)
-
-        # convert to a python pipe instance
-        pipe = Pipe.from_id(pipeline_pk)
-
-        caller_output = pipe.outputs
-        root_pipeline_pk = pipeline_record.root_pipeline_id
-
-        pipe._context = self
-        pipeline_iter = pipe.run()
-
-        next_value = None
-        while True:
-            try:
-                yielded = pipeline_iter.send(next_value)
-            except StopIteration:
-                break
-            except Exception, e:
-                raise
-
-            if isinstance(yielded, Pipe):
-                next_value = PipeFuture(yielded.output_names)
-            else:
-                raise
--- django_p/tasks.py
+++ django_p/tasks.py
@@ -1,6 +1,247 @@
-from .models import PipeContext
+from .models import Pipeline, Slot, Barrier
+from django_q.tasks import async, result
+from datetime import datetime
+import threading
 
 
-def run_pipeline(key, purpose=None, attempt=0):
-    context = PipeContext()
-    context.evaluate(key, purpose, attempt)
+class After(object):
+    """Causes all contained Pipelines to run after the given ones complete.
+
+    Must be used in a 'with' block.
+    """
+
+    _local = threading.local()
+
+    def __init__(self, *futures):
+        """Initializer.
+
+        Args:
+          *futures: PipelineFutures that all subsequent pipelines should follow.
+            May be empty, in which case this statement does nothing.
+        """
+        for f in futures:
+            if not isinstance(f, Future):
+                raise TypeError('May only pass PipelineFuture instances to After(). %r',
+                                type(f))
+        self._futures = set(futures)
+
+    def __enter__(self):
+        """When entering a 'with' block."""
+        After._thread_init()
+        After._local._after_all_futures.extend(self._futures)
+
+    def __exit__(self, type, value, trace):
+        """When exiting a 'with' block."""
+        for future in self._futures:
+            After._local._after_all_futures.remove(future)
+        return False
+
+    @classmethod
+    def _thread_init(cls):
+        """Ensure thread local is initialized."""
+        if not hasattr(cls._local, '_after_all_futures'):
+            cls._local._after_all_futures = []
+
+
+class InOrder(object):
+    """Causes all contained Pipelines to run in order.
+
+    Must be used in a 'with' block.
+    """
+
+    _local = threading.local()
+
+    @classmethod
+    def _add_future(cls, future):
+        """Adds a future to the list of in-order futures thus far.
+
+        Args:
+          future: The future to add to the list.
+        """
+        if cls._local._activated:
+            cls._local._in_order_futures.add(future)
+
+    def __init__(self):
+        """Initializer."""
+
+    def __enter__(self):
+        """When entering a 'with' block."""
+        # Reentrancy checking gives false errors in test mode since everything is
+        # on the same thread, and all pipelines are executed in order in test mode
+        # anyway, so disable InOrder for tests.
+        InOrder._thread_init()
+        assert not InOrder._local._activated, 'Already in an InOrder "with" block.'
+        InOrder._local._activated = True
+        InOrder._local._in_order_futures.clear()
+
+    def __exit__(self, type, value, trace):
+        """When exiting a 'with' block."""
+        InOrder._local._activated = False
+        InOrder._local._in_order_futures.clear()
+        return False
+
+    @classmethod
+    def _thread_init(cls):
+        """Ensure thread local is initialized."""
+        if not hasattr(cls._local, '_in_order_futures'):
+            cls._local._in_order_futures = set()
+            cls._local._activated = False
+
+
+
+class Pipe(object):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        pass
+
+
+class Future(object):
+    def __init__(self, pipe):
+        self._after_all_pipelines = {}
+        self._output_dict = {
+            'default': Slot.objects.update_or_create(filler=pipe, name='default')[1]
+        }
+
+        for name in pipe.output_names:
+            assert name not in self._output_dict
+
+            self._output_dict[name] = Slot.objects.update_or_create(filler=pipe, name=name)[1]
+
+    def __getattr__(self, name):
+        if name not in self._output_dict:
+            raise
+        return self._output_dict[name]
+
+
+def _generate_args(pipeline, future):
+    params = {
+        'args': [],
+        'kwargs': {},
+        'after_all': [],
+        'output_slots': {},
+        'class_path': pipeline._class_path,
+        'backoff_seconds': pipeline.backoff_seconds,
+        'backoff_factor': pipeline.backoff_factor,
+        'max_attempts': pipeline.max_attempts,
+        'task_retry': pipeline.task_retry,
+        'target': pipeline.target,
+    }
+    dependent_slots = set()
+
+    arg_list = params['args']
+    for current_arg in pipeline.args:
+        if isinstance(current_arg, Future):
+            current_arg = current_arg.default
+        if isinstance(current_arg, Slot):
+            arg_list.append({'type': 'slot', 'slot_key': str(current_arg.key)})
+            dependent_slots.add(current_arg.key)
+        else:
+            arg_list.append({'type': 'value', 'value': current_arg})
+
+    kwarg_dict = params['kwargs']
+    for name, current_arg in pipeline.kwargs.iteritems():
+        if isinstance(current_arg, Future):
+            current_arg = current_arg.default
+        if isinstance(current_arg, Slot):
+            kwarg_dict[name] = {'type': 'slot',
+                                'slot_key': str(current_arg.key)}
+            dependent_slots.add(current_arg.key)
+        else:
+            kwarg_dict[name] = {'type': 'value', 'value': current_arg}
+
+    after_all = params['after_all']
+    for other_future in future._after_all_pipelines:
+        slot_key = other_future._output_dict['default'].key
+        after_all.append(str(slot_key))
+        dependent_slots.add(slot_key)
+
+    output_slots = params['output_slots']
+    output_slot_keys = set()
+    for name, slot in future._output_dict.iteritems():
+        output_slot_keys.add(slot.key)
+        output_slots[name] = str(slot.key)
+
+    return dependent_slots, output_slot_keys, params
+
+
+def notify_barriers(slot):
+    for barrier in slot.barrier_set.all():
+        if all(slot.status == Slot.STATUS.FILLED for slot in barrier.blocking_slots.all()):
+            barrier.status = Barrier.STATUS.FIRED
+            barrier.triggered = datetime.utcnow()
+            barrier.save()
+
+            async('evaluate', barrier.target.pk)
+
+
+def fill_slot(filler, slot, value):
+    slot.filler = filler
+    slot.value = value
+    slot.status = Slot.STATUS.FILLED
+    slot.filled = datetime.utcnow()
+    slot.save()
+
+    notify_barriers(slot)
+
+
+def evaluate(pipeline_pk, purpose, attempt=0):
+    # FIXME: After, InOrder
+
+    pipeline = Pipeline.get(pk=pipeline_pk)
+
+    # FIXME: handle not generator case
+    pipeline_iter = pipeline.run()
+
+    last_sub_stage = None
+    sub_stage_dict = {}
+    sub_stage_ordering = []
+    next_value = None
+
+    while True:
+        try:
+            yielded = pipeline_iter.send(next_value)
+        except StopIteration:
+            break
+        except Exception, e:
+            raise
+
+        assert isinstance(yielded, Pipe)
+        assert yielded not in sub_stage_dict
+
+        next_value = Future(yielded)
+        next_value._after_all_pipelines.update(
+            After._local._after_all_futures
+        )
+        next_value._after_all_pipelines.update(
+            InOrder._local._in_order_futures
+        )
+        sub_stage_dict[yielded] = next_value
+        sub_stage_ordering.append(yielded)
+        InOrder._add_future(next_value)
+
+    if last_sub_stage:
+        # FIXME:
+        pass
+
+    for sub_stage in sub_stage_ordering:
+        future = sub_stage_dict[sub_stage]
+
+        dependent_slots, output_slots, params = _generate_args(sub_stage, future)
+
+        child_pipeline = Pipeline.objects.create(
+            root_pipeline=pipeline,
+            params=params,
+            class_path=sub_stage._class_path
+        ).save()
+
+        barrier = Barrier.objects.create(
+            root_pipeline=pipeline,
+            target=child_pipeline,
+            purpose=Barrier.PURPOSE.START,
+            blocking_slots=dependent_slots
+        )
+
+
